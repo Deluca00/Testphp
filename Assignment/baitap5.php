bafadfac0cc94b754e42aa30e4c99fa6bf35238f
Deluca00 <huynhkanh24@gmail.com>
2023-09-12 12:33:27 +0700

    fgfgfgf

---
+++ Assignment/baitap5.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="bai5.php" method="post">
+        <table width="806" border="1">
+            <tr>
+                <td colspan="4" bgcolor="#336699"><strong>Giai phuong trinh bac 2</strong></td>
+            </tr>
+            <tr>
+                <td>Phuong trinh</td>
+                <td width="236">
+                    <input type="text" name="a"> X^2+
+                </td>
+                <td width="218"><label for="textfield3"></label><input type="text" name="b" id="textfield3">X+</td>
+                <td width="241"><label for="textfield"></label><input type="text" name="c" id="textfield">=0</td>
+            </tr>
+            <tr>
+                <td colspan="4">Nghiem
+                    <label for="textfield2"></label>
+                    <input type="text" name="textfield" id="textfield2" width="400" value="">
+                </td>
+            </tr>
+            <tr>
+                <td colspan="4" align="center" valign="middle">
+                    <input type="submit" name="chao" id="chao" value="Xuat">
+                </td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
